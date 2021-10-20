7fdb8f508a2428bb331d270ccd82f42800ae07ff
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-10-20 11:47:58 +0530

    Checkpoint commit

--- lab_4/cpu-scheduling.c
+++ lab_4/cpu-scheduling.c
@@ -4,6 +4,8 @@
 	Author: Srikar
 */
 
+// TODO: Complete implementing it
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -14,19 +16,22 @@ struct proc
 	int arv_time;
 	int bst_time;
 	int proiority;
-}
+	int isRun;
+};
 
 typedef struct proc proc;
 
-int main()
+int main(int argc, char **argv)
 {
+
+	void printAllProcs(proc[], int);
 
 	int input_arr[argc - 1];
 
 	for(int i = 1; i < argc; i++)
 	{
 		for(int j = 0; j < strlen(argv[i]); j++)
-			if(!isdigit(argv[i][j])
+			if(!isdigit(argv[i][j]))
 			{
 				printf("Please enter only numbers!\n");
 				return -1;
@@ -48,14 +53,15 @@ int main()
 
 	for(int i = 0; i < num_proc; i++)
 	{
-		num_proc[i].arv_time = input_arr[i + 2]
-		num_proc[i].bst_time = input_arr[num_proc + i + 2];
+		all_procs[i].arv_time = input_arr[i + 2];
+		all_procs[i].bst_time = input_arr[num_proc + i + 2];
+		all_procs[i].isRun = 0;
 	}
 
 	switch(alg)
 	{
 		case 1:
-				print("FCFS algo lessgoo!\n");
+				printf("FCFS algo lessgoo!\n");
 				break;
 		
 		case 2:
@@ -67,13 +73,28 @@ int main()
 				break;
 
 		case 4:
+				// TODO: Take priorities
 				printf("Priority Scheduling!\n");
 				break;
 
 		case 5:
+				// TODO: take time quantum
 				printf("Round robin scheduling!\n");
 				break;
+
+		default:
+				printf("Incorrect value entered for algorithm! Check and try again.\n");
+				return -1;
 	}
+
+	printAllProcs(all_procs, num_proc);
 	
 	return(0);
+}
+
+void printAllProcs(proc all_procs[], int size)
+{
+	for(int i = 0; i < size; i++)
+		printf("pid =		%d\narv_time = 	%d\nbst_time = 	%d\nproiority = 	%d\nisRun = 	%d\n\n", i, all_procs[i].arv_time, all_procs[i].bst_time, all_procs[i].proiority, all_procs[i].isRun);
+
 }
