0f607ad89334241a7259dffad204ec18cae419cc
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-11-07 18:56:25 +0530

    Dynamic memory allocation for matrices

--- lab_6/bankers.c
+++ lab_6/bankers.c
@@ -5,9 +5,14 @@
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
+	// Funtion prototypes
+	int **getMatrix(int, int);
+
+
 	FILE *inFPtr = NULL;
 
 	inFPtr = fopen("input.txt", "r");
@@ -24,27 +29,30 @@ int main()
 
 	printf("m = %d\nn = %d\n\n", m, n);
 
-	int avail[m], max[n][m], alloc[n][m];
+	// Defining the matrices
+	int **alloc = getMatrix(n, m);
+	int **max = getMatrix(n, m);
+	int **need = getMatrix(n, m);
+	int *avail = (int*)malloc(m * sizeof(int));
 
 	// avail
 	for(int i = 0; i < m; i++)
-		fscanf(inFPtr, "%d ", avail[i]);
-
-	printf("Here1\n");
+		fscanf(inFPtr, "%d ", &avail[i]);
 
 	// max
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < m; j++)
-			fscanf(inFPtr, "%d", max[i][j]);
-
-	printf("Here2\n");
+			fscanf(inFPtr, "%d", &max[i][j]);
 
 	// alloc
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < m; j++)
-			fscanf(inFPtr, "%d", alloc[i][j]);
+			fscanf(inFPtr, "%d", &alloc[i][j]);
 
-	printf("Here3\n");
+	// calculating the need matrix
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			need[i][j] = max[i][j] - alloc[i][j];
 
 	// Printing them:
 
@@ -65,10 +73,29 @@ int main()
 	for(int i = 0; i < n; i++)
 	{
 		for(int j = 0; j < m; j++)
-			printf("%d ", max[i][j]);
+			printf("%d ", alloc[i][j]);
 
 		printf("\n");
 	}
-	
+
+	printf("\nneed:\n");
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+			printf("%d ", need[i][j]);
+
+		printf("\n");
+	}
+
 	return(0);
+}
+
+// Function that returns the pointer to a matrix
+int** getMatrix(int n, int m)
+{
+	int **matrix = (int**)malloc(n * sizeof(int*));
+	for(int i = 0; i < n; i++)
+		matrix[i] = (int*)malloc(m * sizeof(int));
+
+	return matrix;
 }
