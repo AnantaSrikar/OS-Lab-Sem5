70d224dcd08f7418cf9d53be4e16a7404960348a
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-12-06 03:01:24 +0530

    Added random number generator

--- lab_7/mem-alloc.c
+++ lab_7/mem-alloc.c
@@ -8,9 +8,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+#include<time.h>
 
 int main(int argc, char** argv)
 {
+	// Function prototypes
+	int getRandNum(int, int);
+
 	// Initial code to get all command line values
 	
 	if(argc != 3)
@@ -45,8 +49,37 @@ int main(int argc, char** argv)
 	int profile = input_arr[0];
 	int policy = input_arr[1];
 
-	printf("Profile: %d\nPolicy: %d\n", profile, policy);
+	// Seeding the random number generator
+	// NOTE: seed it only ONCE in the code
+	srand(time(0));
 
+	printf("Random number: %d\n", getRandNum(1, 1024));
+	printf("Random number: %d\n", getRandNum(1, 100));
+	printf("Random number: %d\n", getRandNum(500, 1000));
+	
+	switch(policy)
+	{
+		case 1:
+				printf("Use best fit, lessgoo\n");
+				break;
+
+		case 2:
+				printf("Use worst fit, lessgoo\n");
+				break;
+
+		case 3:
+				printf("Use first fit, lessgoo\n");
+				break;
+		
+		default:
+				printf("What did you do!? Something very bad happened!\nTerminating immediately!\n");
+	}
 	
 	return(0);
+}
+
+// Function to generate random numbers in a given range
+int getRandNum(int min, int max)
+{
+	return (rand() % (max - min + 1) + 1);
 }
